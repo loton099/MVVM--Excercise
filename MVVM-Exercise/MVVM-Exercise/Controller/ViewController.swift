5fc821040f9515afb72e3e00aa3f8696c1e2599f
Shakti Prakash <shaktiprakash099@gmail.com>
2021-02-03 17:27:31 +0530

    Basic Project  Set up Done

---
+++ MVVM-Exercise/MVVM-Exercise/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Exercise
+//
+//  Created by Shakti Prakash Srichandan on 03/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
---
+++ MVVM-Exercise/MVVM-Exercise/ViewModel/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  MVVM-Exercise
+//
+//  Created by Shakti Prakash Srichandan on 03/02/21.
+//
+
+import Foundation
