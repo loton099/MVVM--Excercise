3a72b2693735328ae94abed8c83bd71149a722ab
Shakti Prakash <shaktiprakash099@gmail.com>
2021-02-03 18:35:07 +0530

    Model test cases done

--- MVVM-Exercise/MVVM-ExerciseTests/MVVM_ExerciseTests.swift
+++ MVVM-Exercise/MVVM-ExerciseTests/MVVM_ExerciseTests.swift
@@ -9,25 +9,57 @@ import XCTest
 @testable import MVVM_Exercise
 
 class MVVM_ExerciseTests: XCTestCase {
-
+    
+    var company: Company!
+    var geo: Geo!
+    var address: Address!
+    var user: User!
+    
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        company = Company(name: "Apple", catchPhrase: "Multi-layered client-server neural-net", bs: "")
+        geo = Geo(lat: "11.23", lng: "23.11")
+        address = Address(street: "", suite: "999", city: "Cuttack", zipcode: "753004", geo: geo)
+        user = User(id: 1 , name: "shakti", username: "shakti123", email: "shaktiprakash099@gmail.com", address: address, phone: "9040600856", website: "shaktipraksh099@gmail.com", company: company)
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        company = nil
+        geo = nil
+        address = nil
+        user = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    //MARK: Model Test methods
+    
+    func testComapanyModel() throws {
+        XCTAssertEqual(company.name, "Apple")
+        XCTAssertTrue(company.bs.isEmpty, "Yes")
     }
-
+    
+    func testGeoModel() throws {
+        geo = nil
+        XCTAssertNil(geo)
+    }
+    
+    func testAddressModel() throws {
+        XCTAssertEqual(address.geo.lat, geo.lat)
+    }
+    
+    func testUserModel() throws {
+        XCTAssertEqual(user.address.city, address.city)
+        XCTAssertEqual(user.company.name, company.name)
+    }
+    
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
